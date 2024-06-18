1a4f0f828b3daaf6b450bc65eace748b73a33abd
Seif Mahmoud <seifmahmoud871@gmail.com>
2024-06-18 11:29:26 +0300

    add config

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://seifmahmoud:Mongopass@cluster0.yrtwlkk.mongodb.net/MeetingApp').
+    await mongoose.connect(process.env.DB).
     then(result=>{console.log(`DB connected successfully ........`);}).
     catch(err=>`fail to connect on DB ....... ${err}`)
 }
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import express from 'express'
 import initApp from './src/index.router.js'
 import initMeetingServer from './src/meeting-server.js'
 
+import dotenv from 'dotenv'
+import path from 'path'
+import {fileURLToPath} from 'url'
+dotenv.config()
+
 const app = express()
 
-
+const __direname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({path:path.join(__direname,"./config/.env")})
 const port = process.env.PORT || 8000
 
 initApp(app,express);
